24b0c888ba3bafb6e8f7df408a97cd2600a94f33
alex-tm-wrk <61699456+alex-tm-wrk@users.noreply.github.com>
2025-07-15 13:16:37 +0200

    Remove unused DB import from DatabaseSeeder
    The unused Illuminate\Support\Facades\DB import was removed from DatabaseSeeder.php to clean up the code.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
