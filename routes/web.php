31ec07406aa1d6984e96c51dee44adb935e15038
alex-tm-wrk <61699456+alex-tm-wrk@users.noreply.github.com>
2025-08-10 18:11:36 +0200

    Add applicant store and delete functionality
    Implemented methods in ApplicantController to handle job application submissions and applicant deletion. Updated web routes to group applicant-related routes under auth middleware for better access control.

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,16 @@ Route::middleware('guest')->group(function () {
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
-
 Route::middleware('auth')->group(function () {
     Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
     Route::post('/bookmarks/{job}', [BookmarkController::class, 'store'])->name('bookmarks.store');
     Route::delete('/bookmarks/{job}', [BookmarkController::class, 'destroy'])->name('bookmarks.destroy');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])->name('applicant.store');
+    Route::delete('/applicants/{applicant}', [ApplicantController::class, 'destroy'])->name('applicant.destroy');
 });
 
-Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])->name('applicant.store')->middleware('auth');
-Route::delete('/applicants/{applicant}', [ApplicantController::class, 'destroy'])->name('applicant.destroy')->middleware('auth');
+
